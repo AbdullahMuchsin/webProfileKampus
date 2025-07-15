dd39c5532676e389b814b54ba2cddb0c212383b5
Abdullah Muchsin <Abdullahmuchsin96@gmail.com>
2025-07-16 03:59:14 +0700

    Update Kode

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 // START TOP NAVBAR
 document.addEventListener("click", function (e) {
   const dropdownBtn = e.target.closest("[data-dropdown-toggle]");
-  const openMenu = document.querySelector(".language-menu.show");
+  const openMenu = document.querySelector(".language-menu-top-navbar.show");
 
   if (dropdownBtn) {
-    const menu = dropdownBtn.querySelector(".language-menu");
+    const menu = dropdownBtn.querySelector(".language-menu-top-navbar");
     menu.classList.toggle("show");
   } else if (openMenu) {
     openMenu.classList.remove("show");
@@ -19,6 +19,19 @@ document
     var menu = document.getElementById("main-menu");
     menu.classList.toggle("show");
   });
+
+document.addEventListener("DOMContentLoaded", function () {
+  window.addEventListener("scroll", function () {
+    const navbar = document.getElementById("main-navbar");
+    if (window.scrollY > 5) {
+      navbar.style.marginTop = "0";
+      navbar.style.padding = ".85rem 0"; // harus pakai style.
+    } else {
+      navbar.style.padding = "1rem 0";
+      navbar.style.marginTop = "44px";
+    }
+  });
+});
 // END MAIN NAVBAR
 
 // START MAIN CONTENT
